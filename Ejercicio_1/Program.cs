1d8c2e83f8720f7ad4f1c7556d99c9d90ac68b8f
VictoriaShadow <maidanavalentina2008@gmail.com>
2026-06-29 22:57:13 -0300

    Program Completo

--- Ejercicio_1/Program.cs
+++ Ejercicio_1/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Runtime.CompilerServices;
+using AutoDrive;
+List <Vehiculo> vehiculos = new List <Vehiculo> ();
+
+vehiculos.Add (new AutoElectrico{Marca = "BYD", Autonomia = 500});
+vehiculos.Add (new AutoElectrico{Marca = "Renault", Autonomia = 280});
+vehiculos.Add (new Camioneta{Marca = "Toyota", Traccion4x4 = true});
+vehiculos.Add (new Camioneta{Marca = "Volkswagen", Traccion4x4 = false});
+
+foreach (var Vehiculo in vehiculos)
+{
+    string mensaje = Vehiculo.Arrancar();
+    Console.WriteLine(mensaje);
+}
+Console.ReadLine();
